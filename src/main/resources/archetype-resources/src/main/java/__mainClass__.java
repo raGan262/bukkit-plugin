ed7db215200bc4fdcfc865a41e1a32c6debf9dd9
raGan <raGan@raGan-PC>
2014-07-03 15:15:56 +0200

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/__mainClass__.java
@@ -0,0 +1,20 @@
+package ${package};
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ${mainClass} extends JavaPlugin implements Listener {
+	
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
